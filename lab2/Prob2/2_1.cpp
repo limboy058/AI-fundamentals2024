7b6ace5419f8a205ba448ff745fefadd0234087a
limbo-t <2324899904@qq.com>
2024-04-07 10:53:27 +0800

    '交交作业'

--- lab2/Prob2/2_1.cpp
+++ lab2/Prob2/2_1.cpp
@@ -1,115 +1,67 @@
-#include<bits/stdc++.h>//八数码问题 BFS
-
+#include<bits/stdc++.h>//DFS 八数码问题 
 using namespace std;
 #define db double
 #define ll long long
 #define pii pair<int,int>
 #define pll pair<ll,ll>
 
-void check(string s)//检查是否有解
-{
-    if(s.length()!=9)
-    {
-        printf("-1\n");
-        exit(0);
-    }
-    bool yes[9]={0};
-    for(auto x:s)
-    {
-        if(x<'9'&&x>='0')yes[x-'0']=1;
-        if(x=='x')yes[0]=1;
-    }
-    for(int i=0;i<=8;i++)
-    {
-        if(!yes[i])
-        {
-            printf("-1\n");
-            exit(0);
-        }
-    }
-    int nxs=0;//逆序数
-    for(int i=0;i<9;i++)
-        for(int j=i+1;j<9;j++)
-        {
-            char a=s[i],b=s[j];
-            if (a=='x'||b=='x')continue;
-            if(a>b)nxs++;
-        }
-    if(nxs%2==1)
-    {
-        printf("-1\n");
-        exit(0);
-    }
-}
-
 
 void solve()
 {
-    string s = "";
+	string s = "";
+	for (int i = 1; i <= 9; i++)
+	{
+		string tmp;
+		cin >> tmp;
+		s = s + tmp;
+	}
+	set<string>st;//vis
+	pii op[5] = { {0,0}, {0,1} ,{0,-1}, {1,0} ,{-1,0} };
+	stack<string>stk;
 
-    for (int i = 1; i <= 9; i++)
-    {
-        string tmp;
-        cin >> tmp;
-        s = s + tmp;
-    }
+	stk.push(s);
+	while (!stk.empty())
+	{
+		string now = stk.top();
+		stk.pop();
+		if (now == "12345678x")
+		{
+			printf("1");
+			exit(0);
+		}
+		st.insert(now);
+		for (int i = 0; i < 9; i++)
+		{
+			if (now[i] == 'x')
+			{
+				string tmp;
+				int x, y;
+				for (int j = 1; j <= 4; j++)
+				{
+					x = i / 3 + op[j].first;
+					y = i % 3 + op[j].second;
+					if (x <= 2 && x >= 0 && y <= 2 && y >= 0)
+					{
+						tmp = now;
+						swap(tmp[i], tmp[x * 3 + y]);
+						if (!st.count(tmp))
+							stk.push(tmp);
+					}
+				}
+			}
+		}
+	}
 
-    check(s);
-
-    set<string> st;//vis
-    pii op[5] = {{0,  0},
-                 {1,  0},
-                 {0,  1},
-                 {-1, 0},
-                 {0, -1}};//可选的操作，上下左右交换
-    queue<pair<int, string>> q;
-
-    q.emplace(0, s);
-    while (!q.empty())
-    {
-        auto now = q.front();
-        q.pop();
-        if (now.second == "12345678x")
-        {
-            printf("%d\n", now.first);
-            exit(0);
-        }
-        st.insert(now.second);
-        for (int i = 0; i < 9; i++)
-        {
-            if (now.second[i] == 'x')
-            {
-                string tmp;
-                int x, y;
-                for (int j = 1; j <= 4; j++)
-                {
-                    x = i / 3 + op[j].first;
-                    y = i % 3 + op[j].second;
-                    if (x <= 2 && x >= 0 && y <= 2 && y >= 0)
-                    {
-                        tmp = now.second;
-                        swap(tmp[i], tmp[x * 3 + y]);
-                        if (!st.count(tmp))
-                        {
-                            q.emplace(1 + now.first, tmp);
-                        }
-                    }
-
-                }
-            }
-        }
-    }
-
-    printf("-1");
+	printf("0");
 }
 
 signed main()
 {
-    cin.tie(0);
-    cout.tie(0);
-    ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(0);
 
-    solve();
+	solve();
 
-    return 0;
+	return 0;
 }
